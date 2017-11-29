548aebee659a992962fb3f5e9320f45fe4edf0f2
patgrasso <pgrasso@stevens.edu>
2017-11-29 13:20:37 -0500

    Install subdirectory dotfiles individually
    Linking entire dot-directories (.vim, .config, ...) means that anything
    placed in those directories later on (e.g. vim plugins) will be seen by
    git. Instead of manually ignoring each of these items, just link each
    regular file in these subdirectories.

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ NON_DOT_LINKS="
   texmf
 "
 
+# Find absolute path of dotfiles directory (wherever this script is)
 LOCAL_DIR=`pwd`/$(dirname $0)
 [ `basename $LOCAL_DIR` == "." ] && LOCAL_DIR=`pwd`
 
@@ -29,8 +30,16 @@ prompt () {
   return 1
 }
 
+show_warnings () {
+  warnings=$(grep --color=always -i -n -A 3 "WARNING:" "$f")
+  if [ ! -z "$warnings" ]; then
+    echo "$f:"
+    echo -e "$warnings"
+  fi
+}
+
 make_sym_link () {
-  dotfile=$(basename $1)
+  dotfile=$1
   with_dot=${2:-0}
 
   if [ $with_dot == 1 ]; then
@@ -42,7 +51,7 @@ make_sym_link () {
   link_path=$HOME/$link_name
   dotfile_path=$LOCAL_DIR/$dotfile
 
-  if [ $(readlink $link_path) != "$dotfile_path" ]; then
+  if [ "$(readlink -f $link_path)" != "$(readlink -f $dotfile_path)" ]; then
     if [ -L "$HOME/$link_name" ]; then
       prompt "~/$link_name points to $(readlink $link_path). Overwrite?"
       [ $? == 1 ] && return 1;
@@ -50,15 +59,27 @@ make_sym_link () {
       prompt "~/$link_name exists as a regular file. Overwrite?"
       [ $? == 1 ] && return 1;
     else
+      show_warnings "$dotfile"
       echo "Creating link: ~/$link_name -> $dotfile_path"
       ln -s $dotfile_path $link_path
     fi
   fi
 }
 
-for f in $LOCAL_DIR/*; do
-  dotfile=$(basename $f)
-  contains "$IGNORE $NON_DOT_LINKS" "$dotfile"
-  [ $? == 1 ] && make_sym_link $dotfile 1
+# Move into the dotfiles dir
+cd $LOCAL_DIR
+
+# For everything that isn't in NON_DOT_LINKS or IGNORE in the local dir
+for f in $(find * -maxdepth 0 -type f); do
+  contains "$IGNORE" "$f"
+  [ $? == 1 ] && make_sym_link $f 1
+done
+
+# Now, for all NON_DOT_LINKS
+for d in "$NON_DOT_LINKS"; do
+  for f in $(find $d -type f); do
+    mkdir -p "$HOME/$(dirname $f)"
+    make_sym_link $f 0
+  done
 done
 
